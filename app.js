2893eabc0cff55e9817272da4eb99e1d92dd7354
Ronak Vijay <ronaksvijay@gmail.com>
2018-09-25 21:53:56 +0530

    updating the budget

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // BUDGET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
 
-    var Expense = function(id, description, value) {
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    var Income =  function(id, description, value) {
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
+
+    var calculateTotal  = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    }
 
     var data = {
         allItems: {
@@ -21,24 +29,26 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     }
 
     return {
-        addItem: function(type, des, val) {
+        addItem: function (type, des, val) {
             var newItem, ID;
 
             // Create new ID
-            if(data.allItems[type].length > 0) {
+            if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
-            
+
             //Create new item based on the'inc' or 'exp' type
             if (type === 'exp') {
                 newItem = new Expense(ID, des, val);
-            } else if (type ==='inc') {
+            } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
 
@@ -47,13 +57,39 @@ var budgetController = (function() {
 
             //Return the element
             return newItem;
+        },
+
+        calculateBudget: function() {
+            
+            // Calculate total inc and exp
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            //calculate the budget: inc - exp
+            data.budget = data.totals.inc - data.totals.exp; 
+            
+            // Calculate the percentage of inc that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100); 
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         }
     }
 
-})(); 
+})();
 
 // UI CONTRLOLLER
-var UIController = (function() {
+var UIController = (function () {
 
     var DOMstrings = {
         inputType: '.add__type',
@@ -65,51 +101,51 @@ var UIController = (function() {
 
     }
     return {
-        getinput: function() {
+        getinput: function () {
             return {
-                type: document.querySelector(DOMstrings.inputType).value,  //will be either inc or exp
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
-        addListItem: function(obj, type) {
+        addListItem: function (obj, type) {
             var html, newHtml, element;
             // Create HTML string with placeholder text
-            
+
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                
+
                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expenseContainer;
-                
+
                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            
+
             // Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
             newHtml = newHtml.replace('%value%', obj.value);
-            
+
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
-        clearFields: function() {
+        clearFields: function () {
             var fields, fieldsArr;
-            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);    
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
 
             fieldsArr = Array.prototype.slice.call(fields)
 
-            fieldsArr.forEach(function(current, index, array) {
+            fieldsArr.forEach(function (current, index, array) {
                 current.value = "";
             });
 
             fieldsArr[0].focus();
         },
 
-        getDOMstrings: function() {
+        getDOMstrings: function () {
             return DOMstrings;
         }
 
@@ -117,42 +153,56 @@ var UIController = (function() {
 })();
 
 // GLOBAL APP CONTROLLER
-var appController = (function(budgetCtrl, UICtrl) {
+var appController = (function (budgetCtrl, UICtrl) {
 
-    var setUpEventListeners = function() {
+    var setUpEventListeners = function () {
         var DOM = UICtrl.getDOMstrings();
 
         document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
 
-        document.addEventListener('keypress', function(e) {
-            if(e.keyCode === 13 || event.which === 13) {
+        document.addEventListener('keypress', function (e) {
+            if (e.keyCode === 13 || event.which === 13) {
                 controlAddItem();
             }
         })
     }
-    
-    var controlAddItem = function() {
-        var input, newItem;
+
+    var updateBudget = function () {
         
+        //1. Calculate the budget
+        budgetCtrl.calculateBudget();
+
+        //2. Return the budget
+        var budget = budgetCtrl.getBudget();
+
+        //3. Display the budget on the UI
+        console.log(budget);
+    }
+
+    var controlAddItem = function () {
+        var input, newItem;
+
         //1. Get the field Inputdata
         input = UICtrl.getinput();
-        
-        //2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        
-        //3. Add the item in UI
-        UICtrl.addListItem(newItem, input.type);
 
-        //4. Clear the fields
-        UICtrl.clearFields();
-        //4. Calculate the budget
+        if (input.description != "" && !isNaN(input.value) && input.value > 0) {
+            //2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        //5. Display the budget on the UI
+            //3. Add the item in UI
+            UICtrl.addListItem(newItem, input.type);
+
+            //4. Clear the fields
+            UICtrl.clearFields();
+
+            //5. Calculate and update budget
+            updateBudget();
+        }
 
     }
 
     return {
-        init: function() {
+        init: function () {
             console.log('Application has started.');
             setUpEventListeners();
         }
